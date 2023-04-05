847fec1f682724804595d3840c70076f00f2c52b
YuhangQiu00 <yq2347@columbia.edu>
2023-04-05 23:19:53 +0000

    change read data

--- code/read_data_ori.py
+++ code/read_data_ori.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, ConcatDataset
+from torch.utils.data import Dataset
 from transformers import *
 import torch.utils.data as Data
 import pickle
@@ -11,47 +11,18 @@ class Translator:
     """Backtranslation. Here to save time, we pre-processing and save all the translated data into pickle files.
     """
 
-    def __init__(self, path, data_set, transform_type='BackTranslation'):
-        if data_set == 'Yahoo':    
-            # Pre-processed German data
-            with open(path + 'yahoo_train_german.pkl', 'rb') as f:
-                self.de = pickle.load(f)
-            # Pre-processed Russian data
-            with open(path + 'yahoo_train_russian.pkl', 'rb') as f:
-                self.ru = pickle.load(f)
-        
-        elif data_set == 'OOD':
-            # Pre-processed German data
-            with open(path + 'OOD_concatenated_german.pkl', 'rb') as f:
-                self.de = pickle.load(f)
-            # Pre-processed Russian data
-            with open(path + 'OOD_concatenated_russian.pkl', 'rb') as f:
-                self.ru = pickle.load(f)
-        else:
-            pass
-
+    def __init__(self, path, transform_type='BackTranslation'):
+        # Pre-processed German data
+        with open(path + 'de_1.pkl', 'rb') as f:
+            self.de = pickle.load(f)
+        # Pre-processed Russian data
+        with open(path + 'ru_1.pkl', 'rb') as f:
+            self.ru = pickle.load(f)
 
     def __call__(self, ori, idx):
         out1 = self.de[idx]
         out2 = self.ru[idx]
         return out1, out2, ori
-    
-# class Translator_ood:
-#     """Backtranslation. Here to save time, we pre-processing and save all the translated data into pickle files.
-#     """
-
-#     def __init__(self, path, transform_type='BackTranslation'):
-#         # Pre-processed German data
-#         with open(path + 'OOD_concatenated_german.pkl', 'rb') as f:
-#             self.de = pickle.load(f)
-#         # Pre-processed Russian data
-#         with open(path + 'OOD_concatenated_russian.pkl', 'rb') as f:
-#             self.ru = pickle.load(f)
-
-#     def __call__(self, ori, idx):
-#         out1 = self.de[idx]
-#         out2 = self.ru[idx]
-#         return out1, out2, ori
 
 def get_data(data_path, n_labeled_per_class, unlabeled_per_class=5000, max_seq_len=256, model='bert-base-uncased', train_aug=False):
     """Read data, split the dataset, and build dataset for dataloaders.
@@ -70,62 +41,34 @@ def get_data(data_path, n_labeled_per_class, unlabeled_per_class=5000, max_seq_l
     # Load the tokenizer for bert
     tokenizer = BertTokenizer.from_pretrained(model)
 
-    train_df = pd.read_csv(data_path+'reduced_data/yahoo_train.csv', index_col = 0)
-    ood_df = pd.read_csv(data_path+'reduced_data/OOD_concatenated.csv', index_col = 0)
-    test_df = pd.read_csv(data_path+'reduced_data/yahoo_test.csv', index_col = 0)
+    train_df = pd.read_csv(data_path+'train.csv', header=None)
+    test_df = pd.read_csv(data_path+'test.csv', header=None)
 
     # Here we only use the bodies and removed titles to do the classifications
-#     train_labels = np.array([v-1 for v in train_df['labels']])
-#     train_text = np.array([v for v in train_df['content']])
+    train_labels = np.array([v-1 for v in train_df[0]])
+    train_text = np.array([v for v in train_df[2]])
 
-#     ood_text = np.array([v for v in ood_df['content']])
-
-#     test_labels = np.array([u-1 for u in test_df['labels']])
-#     test_text = np.array([v for v in test_df['content']])
-    
-    train_labels = train_df['labels']
-    train_text = train_df['content']
-
-    ood_text = ood_df['content']
-
-    test_labels = test_df['labels']
-    test_text = test_df['content']
+    test_labels = np.array([u-1 for u in test_df[0]])
+    test_text = np.array([v for v in test_df[2]])
 
     n_labels = max(test_labels) + 1
 
     # Split the labeled training set, unlabeled training set, development set
-    # train_labeled_idxs, train_unlabeled_idxs, val_idxs = train_val_split(
-    #     train_labels, n_labeled_per_class, unlabeled_per_class, n_labels)
-
-    train_labeled_idxs = torch.load(data_path+"/indices/msr_30_equal_100_labeled_indices.pt")
-    train_unlabeled_idxs_yahoo = torch.load(data_path+"/indices/msr_30_idr_5_50000_unlabeled_indices.pt")['in_domain_idx']
-    train_unlabeled_idxs_ood = torch.load(data_path+"/indices/msr_30_idr_5_50000_unlabeled_indices.pt")['out_of_domain_idx']
-    val_idxs = torch.load(data_path+"/indices/eval_indices.pt")
-
+    train_labeled_idxs, train_unlabeled_idxs, val_idxs = train_val_split(
+        train_labels, n_labeled_per_class, unlabeled_per_class, n_labels)
 
     # Build the dataset class for each set
     train_labeled_dataset = loader_labeled(
-        train_text.loc[train_labeled_idxs], train_labels.loc[train_labeled_idxs], tokenizer, max_seq_len, train_aug)
-    
-    yahoo_unlabeled_dataset = loader_unlabeled(
-        train_text.loc[train_unlabeled_idxs_yahoo], train_unlabeled_idxs_yahoo, 
-        tokenizer, max_seq_len, Translator(data_path+'all_backtranslations/', 'Yahoo'))
-    
-    ood_unlabeled_dataset = loader_OOD(
-        ood_text.loc[train_unlabeled_idxs_ood], train_unlabeled_idxs_ood, 
-        tokenizer, max_seq_len, Translator(data_path+'all_backtranslations/', 'OOD')
-    )
-
-    train_unlabeled_dataset = ConcatDataset([yahoo_unlabeled_dataset, ood_unlabeled_dataset])
-    
-    
+        train_text[train_labeled_idxs], train_labels[train_labeled_idxs], tokenizer, max_seq_len, train_aug)
+    train_unlabeled_dataset = loader_unlabeled(
+        train_text[train_unlabeled_idxs], train_unlabeled_idxs, tokenizer, max_seq_len, Translator(data_path))
     val_dataset = loader_labeled(
         train_text[val_idxs], train_labels[val_idxs], tokenizer, max_seq_len)
     test_dataset = loader_labeled(
         test_text, test_labels, tokenizer, max_seq_len)
 
-    print("#Labeled: {}, Unlabeled_yahoo {}, Unlabeled_ood {}, Val {}, Test {}".format(len(
-        train_labeled_idxs), len(train_unlabeled_idxs_yahoo), len(train_unlabeled_idxs_ood), len(val_idxs), len(test_labels)))
+    print("#Labeled: {}, Unlabeled {}, Val {}, Test {}".format(len(
+        train_labeled_idxs), len(train_unlabeled_idxs), len(val_idxs), len(test_labels)))
 
     return train_labeled_dataset, train_unlabeled_dataset, val_dataset, test_dataset, n_labels
 
@@ -180,7 +123,7 @@ def train_val_split(labels, n_labeled_per_class, unlabeled_per_class, n_labels, 
     np.random.shuffle(val_idxs)
 
     return train_labeled_idxs, train_unlabeled_idxs, val_idxs
-    
+
 
 class loader_labeled(Dataset):
     # Data loader for labeled data
@@ -241,64 +184,16 @@ class loader_labeled(Dataset):
 
 
 class loader_unlabeled(Dataset):
-    # Data loader for unlabeled data (Yahoo)
+    # Data loader for unlabeled data
     def __init__(self, dataset_text, unlabeled_idxs, tokenizer, max_seq_len, aug=None):
         self.tokenizer = tokenizer
-
         self.text = dataset_text
         self.ids = unlabeled_idxs
-
-        # self.ood_text = ood_text
-        # self.ood_idxs = ood_idxs
-
-        # self.text = self.text.extend(self.ood_text)
-
         self.aug = aug
         self.max_seq_len = max_seq_len
 
     def __len__(self):
         return len(self.text)
-
-    def get_tokenized(self, text):
-        tokens = self.tokenizer.tokenize(text)
-        if len(tokens) > self.max_seq_len:
-            tokens = tokens[:self.max_seq_len]
-        length = len(tokens)
-        encode_result = self.tokenizer.convert_tokens_to_ids(tokens)
-        padding = [0] * (self.max_seq_len - len(encode_result))
-        encode_result += padding
-        return encode_result, length
-
-    def __getitem__(self, idx):
-        if self.aug is not None:
-            u, v, ori = self.aug(self.text[idx], self.ids[idx])
-            encode_result_u, length_u = self.get_tokenized(u)
-            encode_result_v, length_v = self.get_tokenized(v)
-            encode_result_ori, length_ori = self.get_tokenized(ori)
-            return ((torch.tensor(encode_result_u), torch.tensor(encode_result_v), torch.tensor(encode_result_ori)), (length_u, length_v, length_ori))
-        else:
-            text = self.text[idx]
-            encode_result, length = self.get_tokenized(text)
-            return (torch.tensor(encode_result), length)
-        
-class loader_OOD(Dataset):
-    # Data loader for unlabeled data OOD
-    def __init__(self, ood_text, ood_idxs, tokenizer, max_seq_len, aug=None):
-        self.tokenizer = tokenizer
-
-        # self.text = dataset_text
-        # self.ids = unlabeled_idxs
-
-        self.ood_text = ood_text
-        self.ood_idxs = ood_idxs
-
-        self.text = self.ood_text
-
-        self.aug = aug
-        self.max_seq_len = max_seq_len
-
-    def __len__(self):
-        return len(self.ood_text)
 
     def get_tokenized(self, text):
         tokens = self.tokenizer.tokenize(text)
